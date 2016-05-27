f865b57054092540edf98b29d22aaa86412002e0
Samuel Harrison Wolfe <samuelhwolfe@gmail.com>
2016-05-26 23:14:14 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	
+	$('.project-row').magnificPopup({
+		delegate: 'a', // child items selector, by clicking on it popup will open
+		type: 'iframe'
+		// other options
+});
+	
+});
